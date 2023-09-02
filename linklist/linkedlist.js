6198a5153f823f198c1320795487bc0804d8e250
Thanh Nguyen <tv.nguyen9@outlook.com>
2023-09-01 17:36:50 -0700

    Adding unshift method to linkedlist

--- linklist/linkedlist.js
+++ linklist/linkedlist.js
@@ -40,6 +40,7 @@ class LinkedList {
       let temp = this.head
       let pre = this.head
 
+      /* for loop example
       for (let i = 0; i < this.length; i++) {
          if (temp.next) {
             pre = temp
@@ -55,11 +56,42 @@ class LinkedList {
             this.tail = null
          }
       }
+      */
+
+      // while loop example
+      while (temp.next) {
+         pre = temp
+         temp = temp.next
+      }
+      this.tail = pre
+      this.tail.next = null
+      this.length--
+
+      if (this.length === 0) {
+         this.head = null
+         this.tail = null
+      }
       return temp
    }
 
    // unshift
-   unshift(value) { }
+   unshift(value) {
+      // Create a new node
+      const newNode = new Node(value)
+
+      // If head is null point head and tail to newNode
+      if (!this.head) {
+         this.head = newNode
+         this.tail = newNode
+         // If head is not null, point newNode.next to this.head
+         // and point this.head to newNode
+      } else {
+         newNode.next = this.head
+         this.head = newNode
+      }
+      this.length++
+      return this
+   }
 
    // insert 
    insert(index, value) { }
@@ -68,6 +100,8 @@ class LinkedList {
 let myLinkedList = new LinkedList(1)
 myLinkedList.push(2)
 myLinkedList.push(3)
-myLinkedList.pop()
-myLinkedList.pop()
+myLinkedList.push(4)
+
+// myLinkedList.pop()
+// myLinkedList.pop()
 // myLinkedList.pop()
