78a0c43b9aaa78b3fdb06dfe45cdc3f05444f729
Thanh Nguyen <tv.nguyen9@outlook.com>
2023-09-27 14:34:21 -0700

    Adding shift method to linkedlist

--- linklist/linkedlist.js
+++ linklist/linkedlist.js
@@ -92,6 +92,18 @@ class LinkedList {
       return this
    }
 
+   shift() {
+      if (!this.head) return undefined
+      let temp = this.head
+      this.head = this.head.next
+      temp.next = null
+      this.length--
+      if (this.length === 0) {
+         this.tail = null
+      }
+      return temp
+   }
+
    // insert 
    insert(index, value) { }
 }
