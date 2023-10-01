282f2dce35dd66bb7b76edf2811967d8474198d1
Thanh Nguyen <tn@m116.local>
2023-10-01 03:37:23 -0700

    Adding remove method to linkedlist

--- linklist/linkedlist.js
+++ linklist/linkedlist.js
@@ -139,12 +139,28 @@ class LinkedList {
       this.length++
       return true
    }
+
+   // remove
+   remove(index) {
+      if (index === 0) return this.shift()
+      if (index === this.length - 1) return this.pop()
+      if (index < 0 || index >= this.length) return undefined
+
+      const before = this.get(index - 1)
+      const temp = before.next
+
+      before.next = temp.next
+      temp.next = null
+      this.length--
+      return temp
+   }
 }
 
 let myLinkedList = new LinkedList(10)
 myLinkedList.push(11)
 myLinkedList.push(12)
 myLinkedList.push(13)
+
 // insert
 myLinkedList.insert(4, 14)
 
