78a8767b90080e801ceaa5eccd9033769f1238b1
Thanh Nguyen <tv.nguyen9@outlook.com>
2023-10-02 20:56:51 -0700

    Add splice method to array

--- array/arrayjs.js
+++ array/arrayjs.js
@@ -7,9 +7,18 @@ console.log(basicArray)
 
 // pop removes the last item in the array
 basicArray.pop() // O(1)
-
 console.log(basicArray)
 
 // unshift adds a new item to the begining of the array
-basicArray.unshift('Q')
+basicArray.unshift('Q') // O(n)
+basicArray.unshift('X') // O(n)
+console.log(basicArray)
+
+// access to array data
+console.log(basicArray[1]) // O(1)
+
+// splice 
+basicArray.splice(3, 0, 'splice') // O(n)
+console.log(basicArray)
+basicArray.splice(3, 2, 'remove two items after index 3', 'A', 'B', 'C') // O(n)
 console.log(basicArray)
